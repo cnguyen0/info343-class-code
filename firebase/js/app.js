8052a4a4d09daee34a457482fcb703b599ae7255
Cindy Nguyen <cnguyen0@uw.edu>
2015-11-05 16:58:37 -0800

    cri again

--- firebase/js/app.js
+++ firebase/js/app.js
@@ -1,6 +1,28 @@
 
+/*
+theres an API for syncronized arrays for firebase
+*/
+
 angular.module('ChatApp', ['firebase'])
     .constant('firebaseUrl', 'https://info343chat.firebaseio.com/messages')
-    .controller('ChatController', function($scope) {
-        //TODO: implement our chat controller
+    .controller('ChatController', function($scope, $firebaseArray, firebaseUrl) {
+        // create a reference to the firebase
+        var ref = new Firebase(firebaseUrl);
+        ref.limitToLast(1000);
+        $scope.messages = $firebaseArray(ref);
+
+        //initialize form fields
+        $scope.body = null;
+        $scope.name = null;
+
+        $scope.sendMessage = function() {
+            // adds a new object to the array and synchronizes with the server
+            $scope.messages.$add({
+                name: $scope.name,
+                body: $scope.body,
+                createdAt: Firebase.ServerValue.TIMESTAMP // special value
+            });
+
+            $scope.body = null;
+        }
     });
